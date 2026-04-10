0b2405904eea7ca50532fd87754fdad605c725ac
Guillermo-8a <lochoag16@gmail.com>
2026-04-09 21:14:03 -0600

    Update ModuleDistribucion.jsx

--- src/modules/ModuleDistribucion.jsx
+++ src/modules/ModuleDistribucion.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useDispatch, useGlobal, globalActions } from '../context/GlobalContext';
 
 // ============================================================================
