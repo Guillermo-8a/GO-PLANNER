dadc46c572e04e52f400cb4a9d8ea3422b65c600
Guillermo-8a <lochoag16@gmail.com>
2026-04-09 21:29:28 -0600

    Update ModuleReplenishment.jsx

--- src/modules/ModuleReplenishment.jsx
+++ src/modules/ModuleReplenishment.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { useDispatch, useGlobal, globalActions } from '../context/GlobalContext';
+import { Menu } from 'lucide-react';
 
 // --- Motores Matemáticos Inteligencia V4 (Restaurados y Blindados) ---
 const engines = {
