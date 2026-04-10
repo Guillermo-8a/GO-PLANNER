db64e5dfc936a5c11100527576efdb4406aac49a
Guillermo-8a <lochoag16@gmail.com>
2026-04-09 18:43:26 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración para que Vite entienda React y Tailwind
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 })
