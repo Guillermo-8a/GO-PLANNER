785d16e40aa3ac02161a3b5131fae26b213c2078
Guillermo-8a <lochoag16@gmail.com>
2026-04-09 22:08:11 -0600

    Update ModuleDistribucion.jsx

--- src/modules/ModuleDistribucion.jsx
+++ src/modules/ModuleDistribucion.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect,useRef } from 'react';
-import { Menu, Settings, Store, Package, Upload, ArrowUpDown, Sliders, Layers, MoreVertical, Sun, Moon, Info, Map, Database, Activity } from 'lucide-react';
+import { Menu, Settings, Store, Package, Upload, ArrowUpDown, Sliders, Layers, MoreVertical, Sun, Moon, Info, Map, Database, Edit3, Trash2, Save, Rocket, X, Calendar, Trophy, FileSpreadsheet, Check, Copy, ShoppingCart, Settings2, Zap } from 'lucide-react';
 import { useGlobal, useDispatch, globalActions } from '../context/GlobalContext';
 
 // ============================================================================
