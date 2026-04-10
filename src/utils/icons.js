640c4e0cded52d7b0821438feab67935c43cc739
Guillermo-8a <lochoag16@gmail.com>
2026-04-10 07:41:25 -0600

    Create icons.js

---
+++ src/utils/icons.js
@@ -0,0 +1,12 @@
+// src/utils/icons.js
+
+export {
+  TrendingUp, Package, ShoppingCart, BarChart2, Box, Database, RefreshCw,
+  Search, Calendar, Filter, CheckCircle2, AlertCircle, Upload, Download,
+  Settings, FileText, Table, Sun, Moon, Menu,
+
+  Plus, Store, Activity, CheckSquare, BarChart3,
+  MapIcon, FileSpreadsheet, Edit3, Trash2, Rocket,
+  Save, Copy, Check, Trophy, Zap, Settings2
+
+} from 'lucide-react';
