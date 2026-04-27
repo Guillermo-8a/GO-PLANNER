6c1d90baf73828d83c28a7fdab5c23c3968181f9
Guillermo-8a <lochoag16@gmail.com>
2026-04-27 17:28:59 -0600

    Update icons.js

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,14 +1,14 @@
 // src/utils/icons.js
 
 export {
-  Activity, AlertCircle, ArrowUpDown,
+  Activity, AlertCircle, ArrowUpDown, ArrowLeftRight,
   BarChart2, BarChart3,  Box,
   Calendar, Check, CheckCircle2, CheckSquare, ChevronDown,
   ChevronUp, Copy, Calculator,
   Database, Download,
   Edit3,
   FileSpreadsheet, FileText, Filter,
-  Layers, ListPlus,
+  Layers, ListPlus, Loader,
   MapIcon as MapIcon, Menu, Moon, Map,
   Package, Plus,
   RefreshCw, Rocket,
