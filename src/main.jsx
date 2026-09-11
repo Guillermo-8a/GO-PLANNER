15812356943df695f1e0b7fedd6852e0021fbe14
Guillermo-8a <lochoag16@gmail.com>
2026-09-11 00:45:33 -0600

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import TeamTrackerPage from './TeamTrackerPage.jsx'
 import './Index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/team-tracker" element={<TeamTrackerPage />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
