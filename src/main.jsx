9671d7f300748b71c03786e79475e90719f079a1
Guillermo-8a <lochoag16@gmail.com>
2026-09-14 14:09:42 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import TeamTrackerPage from './TeamTrackerPage.jsx'
+import AboutPage from './AboutPage.jsx'
 import './Index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/team-tracker" element={<TeamTrackerPage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
